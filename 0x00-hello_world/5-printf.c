be58bf99fafec50cb80601bb6280975ca4b6a8e1
GalalMohammed <galalmoh2001@gmail.com>
2023-02-17 01:14:54 +0200

    fix a typo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-  * main - Prints "with proper grammer, but the outcome is a piece of art,\n"
+  * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
   *
   * Return: 0
   */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
