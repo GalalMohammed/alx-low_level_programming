a36fcb59a7dd2ea6f28e701abecfcc63a764c2c4
GalalMohammed <galalmoh2001@gmail.com>
2023-02-18 00:15:46 +0200

    add task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+  * main - Prints combs of 4 different digits
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int i = '0', j, k, l;
+
+	for (; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = i; k <= '9'; k++)
+			{
+				for (l = (k == i? j + 1 : '0'); l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == '9' && j == '8' && k == '9' && l == '9' )
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
