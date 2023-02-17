ca5f7a66848066c47a3db70dc8748c295528d007
GalalMohammed <galalmoh2001@gmail.com>
2023-02-17 22:41:41 +0200

    add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Prints 0-9 then '\n'
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	short int c = '0';
+	for (; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
