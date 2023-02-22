a048fbd783214210b40980f055baf3f3fef493d9
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 10:59:36 +0200

    add task 10 and update main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - adds two ints
+  * @n1: first int
+  * @n2: second int
+  *
+  * Return: result of addition
+ */
+int add(int n1, int n2)
+{
+	return n1 + n2;
+}
