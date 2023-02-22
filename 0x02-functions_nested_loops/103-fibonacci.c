f78f366ddd88a0f1aaf1c584414d55147ea307a7
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 22:36:19 +0200

    add task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sum of the even terms
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	long int prev = 1, cur = 1, tmp, sum = 0;
+
+	for (;cur <= 4000000;)
+	{
+		if (cur % 2 == 0)
+			sum += cur;
+		tmp = cur;
+		cur = cur + prev;
+		prev = tmp;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
