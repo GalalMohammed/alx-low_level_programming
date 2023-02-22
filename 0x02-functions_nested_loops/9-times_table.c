bdf5615792c9abbaae169812647ad951c0d87d83
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 10:54:37 +0200

    fix format ot out

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,20 +13,21 @@ void times_table(void)
 		{
 			if (i * j < 10)
 			{
+				if (j > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar('0' + i * j);
 			}
 			else
 			{
-				_putchar('\b');
+				_putchar(' ');
 				_putchar('0' + i * j / 10);
 				_putchar('0' + i * j % 10);
 			}
 			if (j < 9)
-			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
 			else
 				_putchar('\n');
 		}
