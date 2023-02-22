ba99e7f2f55106c6f6a9d138e957f877d4d20790
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 10:49:10 +0200

    add task 9 and update main.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * times_table - Prints 9 times table
+  */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i * j < 10)
+			{
+				_putchar('0' + i * j);
+			}
+			else
+			{
+				_putchar('0' + i * j / 10);
+				_putchar('0' + i * j % 10);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+	}
+}
