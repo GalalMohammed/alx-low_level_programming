0ccb66d160b5b99fd2ab54bd756d01c420593cea
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 10:17:45 +0200

    fix printing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,26 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			if (i < 10)
+			{
 				_putchar('0');
-			_putchar('0' + i);
+				_putchar('0' + i);
+			}
+			else
+			{
+				_putchar('0' + i / 10);
+				_putchar('0' + i % 10);
+			}
 			_putchar(':');
 			if (j < 10)
+			{
 				_putchar('0');
-			_putchar('0' + j);
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
 			_putchar('\n');
 		}
 	}
