bde174f49dc403de254ab9334b35db6335f8d7aa
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 22:23:08 +0200

    add task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the first 50 Fibonacci nums
+  *
+  * Retrun: 0
+  */
+int main(void)
+{
+	int prev = 1, cur = 1, i, tmp;
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%d", cur);
+		tmp = cur;
+		cur = cur + prev;
+		prev = tmp;
+		if (i < 49)
+			printf(", ");
+	}
+	return (0);
+}
