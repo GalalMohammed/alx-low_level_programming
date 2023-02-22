66c18bb9109cc88608530a4139fcf96106b86d04
GalalMohammed <galalmoh2001@gmail.com>
2023-02-22 09:47:06 +0200

    add task 7 and update main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_last_digit - the name is very descriptive
+  * @n: num to be computed
+  *
+  * Return: last digit of n
+  */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
