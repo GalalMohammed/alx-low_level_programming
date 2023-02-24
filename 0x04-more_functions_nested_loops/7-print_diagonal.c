f49618e6a78c0923a35e9e25ae1d6720e5664ae8
GalalMohammed <galalmoh2001@gmail.com>
2023-02-25 00:15:41 +0200

    fix some format

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
   * print_diagonal - draws a diagonal line
-  * @n: num of times of \
+  * @n: num of times of '\'
   */
 void print_diagonal(int n)
 {
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		for(c = 0; c < i; c++)
+		for (c = 0; c < i; c++)
 		{
 			_putchar(' ');
 		}
