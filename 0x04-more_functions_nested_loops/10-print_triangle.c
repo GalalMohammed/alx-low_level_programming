e9e534f45bbacf397652e63c3c4a895bfb00907b
GalalMohammed <galalmoh2001@gmail.com>
2023-02-25 00:59:25 +0200

    add task 10 and update main.h

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_traingle - Yes! You got it right!
+  * @size: that's right too. WOW, you are so smart
+  */
+void print_triangle(int size)
+{
+	int i, s, t;
+
+	for (i = size; i > 0; i--)
+	{
+		for (s = i - 1; s > 0; s--)
+		{
+			_putchar(' ');
+		}
+		for (t = 0; t <= size - i; t++)
+		{
+			_putchar('#');
+		}
+		if ( i != 1)
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
