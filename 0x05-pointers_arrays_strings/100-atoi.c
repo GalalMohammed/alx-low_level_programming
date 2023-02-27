3c942afe65417464f286e65e40dbe8cf75c93199
GalalMohammed <galalmoh2001@gmail.com>
2023-02-27 21:36:08 +0200

    add task 100 and update main.h

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * _atoi - convert str to int
+  * @s: str to be converted
+  *
+  * Return: int
+  */
+int _atoi(char *s)
+{
+	char sign = 1, numFound = 0;
+	int num = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if ( *s >= '0' && *s <= '9')
+		{
+			numFound = 1;
+			num = num * 10 + *s - '0';
+		}
+		else if (numFound)
+		{
+			break;
+		}
+	}
+	return (sign * num);
+}
