b9bb4fec8bfebc110e85a72e7e201c7dcb80c409
GalalMohammed <galalmoh2001@gmail.com>
2023-02-27 20:18:37 +0200

    add task 6 and update main.h

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * puts2 - prints odd chars, followed by a new line
+  * @str: str to be printed
+  */
+void puts2(char *str)
+{
+	char flag = 1;
+
+	while (*str != '\0')
+	{
+		if (flag)
+		{
+			_putchar(*str);
+			flag = 0;
+		}
+		else
+		{
+			flag = 1;
+		}
+	}
+}
