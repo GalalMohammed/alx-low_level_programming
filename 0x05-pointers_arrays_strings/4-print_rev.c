66a1213850e7c6231df00e113725aa67e22714ac
GalalMohammed <galalmoh2001@gmail.com>
2023-02-27 19:26:29 +0200

    add task 4 and update main.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_rev - prints rev of str, followed by a new line
+  * @s: str to be rev
+  */
+void print_rev(char *s)
+{
+	char *s_end = s;
+
+	while (s_end != '\0')
+	{
+		s_end++;
+	}
+	while (s_end >= s)
+	{
+		_putchar(*s_end);
+		s_end--;
+	}
+	_putchar('\n');
+}
