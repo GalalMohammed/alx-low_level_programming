4a34e0359fa15c0ca71034c1a1649a0889dd57b8
GalalMohammed <galalmoh2001@gmail.com>
2023-03-01 22:33:15 +0200

    add task 2 and update main.h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strncpy - copies a string
+  * @dest: pointer to destination
+  * @src: str to be copied
+  * @n: bytes to be copied
+  *
+  * Return: pointer to dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *destcpy = dest;
+
+	while (*src != '\0' && n--)
+	{
+		*destcpy = *src;
+		src++;
+		destcpy++;
+	}
+	*destcpy = '\0';
+	return (dest);
+}
