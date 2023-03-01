acdc4d4cdbc4247afe889e7179d0eb95d5f1bd76
GalalMohammed <galalmoh2001@gmail.com>
2023-03-01 23:59:34 +0200

    change return criteria

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,14 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	unsigned int diff = 0;
-
-	while (*s1 != '\0' || *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		diff = *s1 - *s2;
-		if (*s1 != '\0')
+		if (*s1 != *s2)
 		{
-			s1++;
+			break;
 		}
-		if (*s2 != '\0')
-		{
-			s2++;
-		}
+		s1++;
+		s2++;
 	}
-	return (diff);
+	return (*s1 - *s2);
 }
