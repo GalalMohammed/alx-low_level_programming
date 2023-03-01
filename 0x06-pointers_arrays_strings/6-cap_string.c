1240a276c5aceac21ace53baff33fb5b0e5915a1
GalalMohammed <galalmoh2001@gmail.com>
2023-03-02 01:37:34 +0200

    add task 6 and update main.h

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * cap_string - capitalizes all words
+  * @str: str to be manibulated
+  *
+  * Return: pointer to str
+  */
+char *cap_string(char *str)
+{
+	char *cpy = str;
+
+	*cpy -= *cpy >= 97 && *cpy < 97 + 26 ? 32 : 0;
+	cpy++;
+	while (*cpy != '\0')
+	{
+		if (*cpy == ' ' || *cpy == '\n' || *cpy == '\t' || *cpy == '.')
+		{
+			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+			{
+			*cpy -= 32;
+			}
+		}
+		if (*cpy == ',' || *cpy == ';' || *cpy == '!' || *cpy == '?')
+		{
+			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+			{
+			*cpy -= 32;
+			}
+		}
+		if (*cpy == '"' || *cpy == '(' || *cpy == ')' || *cpy == '{')
+		{
+			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+			{
+			*cpy -= 32;
+			}
+		}
+		if (*cpy == '}')
+		{
+			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+			{
+			*cpy -= 32;
+			}
+		}
+		cpy++;
+	}
+	return (str);
+}
