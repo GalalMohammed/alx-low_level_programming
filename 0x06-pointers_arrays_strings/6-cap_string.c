1f792f90492ac4de37747d510ce3cdce0caa541a
GalalMohammed <galalmoh2001@gmail.com>
2023-03-02 02:08:44 +0200

    rm additional lines

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,31 @@ char *cap_string(char *str)
 	*cpy -= *cpy >= 97 && *cpy < 97 + 26 ? 32 : 0;
 	cpy++;
 	while (*cpy != '\0')
-	{
-		if (*cpy == ' ' || *cpy == '\n' || *cpy == '\t' || *cpy == '.'\
-				|| *cpy == ',' || *cpy == ';' || *cpy == '!'\
-				|| *cpy == '?' || *cpy == '"' || *cpy == '('\
-				|| *cpy == ')' || *cpy == '{' || *cpy == '}')
+		if (*cpy == ' ' || *cpy == '\n' || *cpy == '\t' || *cpy == '.')
 		{
 			cpy++;
 			if (*cpy >= 97 && *cpy < 97 + 26)
-			{
-			*cpy -= 32;
-			}
+				*cpy -= 32;
 		}
-		else
+		else if (*cpy == ',' || *cpy == ';' || *cpy == '!' || *cpy == '?')
 		{
 			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+				*cpy -= 32;
 		}
-	}
+		else if (*cpy == '(' || *cpy == ')' || *cpy == '{' || *cpy == '}')
+		{
+			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+				*cpy -= 32;
+		}
+		else if (*cpy == '"')
+		{
+			cpy++;
+			if (*cpy >= 97 && *cpy < 97 + 26)
+				*cpy -= 32;
+		}
+		else
+			cpy++;
 	return (str);
 }
