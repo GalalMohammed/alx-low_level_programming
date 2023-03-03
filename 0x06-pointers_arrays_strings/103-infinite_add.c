b1989b714cdbb9e3979f3ace51d628dd449d2c38
GalalMohammed <galalmoh2001@gmail.com>
2023-03-04 01:11:25 +0200

    fix style and returning

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,62 @@
 #include "main.h"
 
 /**
+  * numcounter - counts digits
+  * @n: num
+  *
+  * Return: result
+  */
+int numcounter(char *n)
+{
+	int ncounter = 0;
+
+	while (*n != '\0')
+	{
+		n++;
+		ncounter++;
+	}
+	return (ncounter);
+}
+
+/**
+  * zeroR - make r = 0
+  * @r: buffer
+  */
+void zeroR(char *r)
+{
+	*r = '0';
+	*(r + 1) = '\0';
+}
+
+/**
+  * handleCarry - handles carry
+  * @size_r: buffer size
+  * @maxlen: biggest num len
+  * @rcpy: copy of buffer pointer
+  * @carry: carry amount
+  *
+  * Return: 0 if overflow, 1 otherwise
+  */
+int handleCarry(int size_r, int maxlen, char *rcpy, int carry)
+{
+	if (size_r >= maxlen + 2)
+	{
+		while (*rcpy != '\0')
+			rcpy++;
+		while (rcpy >= r)
+		{
+			*(rcpy + 1) = *rcpy;
+			rcpy--;
+		}
+		rcpy++;
+		*rcpy = '0' + carry;
+		return (1);
+	}
+	else
+		return (0);
+}
+
+/**
   * infinite_add - 4dds 2 numb3rs
   * @n1: firs7 num
   * @n2: s3cond num
@@ -11,27 +67,15 @@
   */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *n1cpy = n1, *n2cpy = n2, *rcpy = r;
-	int n1counter = 0, n2counter = 0, res, carry = 0, op1 = 0, op2 = 0,
+	int n1counter, n2counter = 0, res, carry = 0, op1 = 0, op2 = 0,
 	    maxlen = 0;
 
-	*r = '0';
-	*(r + 1) = '\0';
-	while (*n1cpy != '\0')
-	{
-		n1cpy++;
-		n1counter++;
-	}
-	n1cpy--;
-	while (*n2cpy != '\0')
-	{
-		n2cpy++;
-		n2counter++;
-	}
-	n2cpy--;
+	zeroR(r);
+	n1counter = numcounter(n1);
+	n2counter = numcounter(n2);
 	maxlen = n1counter > n2counter ? n1counter : n2counter;
 	if (size_r < n1counter + 1 || size_r < n2counter + 1)
-		return (0);
+		return (r);
 	if (n1counter >= n2counter)
 		*(r + n1counter + 1) = '\0';
 	else
@@ -59,22 +103,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			*(r + n2counter) = '0' + res % 10;
 	}
-	if (carry)
-	{
-		if (size_r < maxlen + 2)
-			return (0);
-		else
-		{
-			while (*rcpy != '\0')
-				rcpy++;
-			while (rcpy >= r)
-			{
-				*(rcpy + 1) = *rcpy;
-				rcpy--;
-			}
-			rcpy++;
-			*rcpy = '0' + carry;
-		}
-	}
+	if (carry && handleCarry(size_r, maxlen, r, carry) == 0)
+		zeroR(r);
 	return (r);
 }
