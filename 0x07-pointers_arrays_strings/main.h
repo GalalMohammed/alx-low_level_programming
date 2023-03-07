bd9cda5546a154eb93043edb4e47e8385724d2c4
GalalMohammed <galalmoh2001@gmail.com>
2023-03-07 22:46:45 +0200

    add task 0, main.h and README.md

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,51 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int);
+void print_times_table(int);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+void reset_to_98(int*);
+void swap_int(int*, int*);
+int _strlen(char*);
+void _puts(char*);
+void print_rev(char*);
+void rev_string(char*);
+void puts2(char*);
+void puts_half(char*);
+void print_array(int*, int);
+char *_strcpy(char*, char*);
+int _atoi(char*);
+char *_strcat(char*, char*);
+char *_strncat(char*, char*, int);
+char *_strncpy(char*, char*, int);
+int _strcmp(char*, char*);
+void reverse_array(int*, int);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *leet(char*);
+char *rot13(char*);
+void print_number(int);
+char *infinite_add(char*, char*, char*, int);
+char *_memset(char*, char, unsigned int);
+
+#endif /* MAIN_H */
