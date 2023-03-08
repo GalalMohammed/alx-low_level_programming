320f036196c6a64c1a1461d6405c501a0806e0d5
GalalMohammed <galalmoh2001@gmail.com>
2023-03-08 20:14:40 +0200

    add task 4 and update main.h

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - computes x ^ y
+  * @x: num
+  * @y: power
+  *
+  * Return: x ^ y
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
