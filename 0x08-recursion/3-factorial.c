58fe0393f14ec8a328edf4dcd5d7594166109af7
GalalMohammed <galalmoh2001@gmail.com>
2023-03-08 10:33:42 +0200

    add task 3 and update main.h

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * factorial - copmutes factorial of num
+  * @n: num to be computed
+  *
+  * Return : factorial of num
+  */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 1)
+	{
+		return (n * factorial(--n));
+	}
+	else
+	{
+		return (1);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -58,5 +58,6 @@ void set_string(char**, char*);
 void _puts_recursion(char*);
 void _print_rev_recursion(char*);
 int _strlen_recursion(char*);
+int factorial(int);
 
 #endif /* MAIN_H */
