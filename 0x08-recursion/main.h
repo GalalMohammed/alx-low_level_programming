37b7e19912c708be09b8e65036b6c4cbd5d57d39
GalalMohammed <galalmoh2001@gmail.com>
2023-03-08 10:19:11 +0200

    add task 2 and update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -57,5 +57,6 @@ void print_diagsums(int*, int);
 void set_string(char**, char*);
 void _puts_recursion(char*);
 void _print_rev_recursion(char*);
+int _strlen_recursion(char*);
 
 #endif /* MAIN_H */
