200d719153bc66dc95689c40ce656c9301d0c85c
GalalMohammed <galalmoh2001@gmail.com>
2023-03-14 14:43:48 +0200

    fix size and null case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s1cpy = s1, *s2cpy = s2, *str, *strcpy;
 	int len1 = 0, len2 = 0, len;
+
 	if (s1 != (void *)'\0')
 	{
 		while (*s1cpy != '\0')
@@ -32,7 +33,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (len1 || len2)
 	{
-		str = (char *)malloc((len1 + len2) * sizeof(char));
+		str = (char *)malloc((len1 - 1 + len2) * sizeof(char));
 		strcpy = str;
 		len = len1 + len2;
 		s1cpy = s1;
@@ -54,10 +55,11 @@ char *str_concat(char *s1, char *s2)
 			strcpy++;
 		}
 		*strcpy = '\0';
-		return (str);
 	}
 	else
 	{
-		return ((void *)'\0');
+		str = (char *)malloc(sizeof(char));
+		str[0] = '\0';
 	}
+	return (str);
 }
