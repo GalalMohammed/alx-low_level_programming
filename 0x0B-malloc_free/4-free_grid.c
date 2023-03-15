b1a764a4b243e7276c0adaab9860e39e4f8c5e94
GalalMohammed <galalmoh2001@gmail.com>
2023-03-15 08:15:04 +0200

    add task 4 and update main.h

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - frees a 2d grid
+  * @grid: 2d array
+  * @height: height of grid
+  */
+void free_grid(int **grid, int height)
+{
+	if (grid != (void *)'\0' && height > 0)
+	{
+		while (height--)
+		{
+			free(grid[height]);
+		}
+		free(grid[0]);
+		free(grid);
+	}
+}
