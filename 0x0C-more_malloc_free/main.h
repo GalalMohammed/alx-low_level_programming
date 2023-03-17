23d974cdb89b1719987265fca9a0ed774a58291a
GalalMohammed <galalmoh2001@gmail.com>
2023-03-17 10:31:11 +0200

    add task 3 and update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -71,5 +71,6 @@ char **strtow(char *);
 void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
 
 #endif /* MAIN_H */
