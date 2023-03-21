302e7ee3220192161ea88dc469de15426cefca20
GalalMohammed <galalmoh2001@gmail.com>
2023-03-21 10:33:06 +0200

    add task 0 and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - new type
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* ifndef DOG_H */
