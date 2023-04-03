1906f506b5227c2b6e5689f5e439c5890b5682b8
GalalMohammed <galalmoh2001@gmail.com>
2023-04-03 23:28:15 +0200

    add task 3 and update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,7 +20,7 @@ int _putchar(char);
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *);
 listint_t *add_nodeint(listint_t **, const int);
-listint_t *add_node_end(listint_t **, const char *);
+listint_t *add_nodeint_end(listint_t **, const int);
 void free_listint(listint_t *);
 
 #endif /* ifndef LISTS_H */
