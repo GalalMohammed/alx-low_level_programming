082f7cc69e955582bca776e26fd3263b77bcf45a
GalalMohammed <galalmoh2001@gmail.com>
2023-04-11 00:36:14 +0200

    add task 100 and update main.h

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * clear_bit - clears value of a bit
+  * @n: num
+  * @index: bit num
+  *
+  * Return: 1 if succes, -1 otherwise
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (8 * 8 - 1))
+	{
+		return (-1);
+	}
+	else
+	{
+		*n &= ~(1 << index);
+		return (1);
+	}
+}
