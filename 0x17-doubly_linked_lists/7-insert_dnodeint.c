024a5224da3ff83f7474ddf5cc1b319af3a669fe
GalalMohammed <galalmoh2001@gmail.com>
2023-05-20 01:19:50 +0300

    reduce var name len

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,22 +3,22 @@
 
 /**
   * insert_dnodeint_at_index - inserts a new node at given pos
-  * @head: head of list
+  * @h: head of list
   * @idx: pos
   * @n: data
   *
   * Return: pointer to new node, NULL if it failed
   */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = -1;
 	dlistint_t *ptr = (void *)'\0';
 
 	if (idx == 0)
-		ptr = add_dnodeint(head, n);
-	else if (head)
+		ptr = add_dnodeint(h, n);
+	else if (h)
 	{
-		ptr = *head;
+		ptr = *h;
 		while (ptr && i + 2 < idx)
 		{
 			ptr = ptr->next;
