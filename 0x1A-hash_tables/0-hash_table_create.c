e3055ac73c1e3b875c2c8aba0f900015c0c21797
GalalMohammed <galalmoh2001@gmail.com>
2023-06-17 21:52:25 +0300

    handle malloc return and fix malloc size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,15 +6,17 @@
  *
  * Return: pointer to hash table
  */
-hash_table_t *hash_table_create(unsigned long int size)
+hash_table_t* hash_table_create(unsigned long int size)
 {
+	size_t i;
 	/* creates a new hash_table */
 	hash_table_t* table = malloc(sizeof(hash_table_t));
-	size_t i;
 
+	if (!table)
+		return (NULL);
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t*));
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
-	return table;
+	return (table);
 }
