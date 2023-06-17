dfa9dc478d9d3b678469a3161a8c2992aa011ce5
GalalMohammed <galalmoh2001@gmail.com>
2023-06-17 21:54:15 +0300

    fix code style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,16 +6,16 @@
  *
  * Return: pointer to hash table
  */
-hash_table_t* hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	size_t i;
 	/* creates a new hash_table */
-	hash_table_t* table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t*));
+	table->array = malloc(size * sizeof(hash_node_t *));
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 	return (table);
